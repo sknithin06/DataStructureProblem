fb2b4bbfdeec20a37451b71c75492510e0528fb6
sknithin06 <nithinsk939@gmail.com>
2021-07-16 20:36:44 +0530

    create a simple linked test

--- LinkedList.java
+++ LinkedList.java
@@ -1,6 +1,52 @@
 package bridgelabz;
 public class LinkedList {
+	class Node {
+		int num;
+		Node next;
+		
+		public Node(int num) {
+			this.num = num;
+			this.next = null;
+		}
+	}
+	public Node head = null;
+	public Node tail = null;
+	
+	public void addNode(int num) {
+		Node newNode = new Node(num);
+		 
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+	}
+	public void display() {
+		Node current = head;
+
+		if(head == null) {
+			System.out.println("List is empty");
+			return;
+		}
+		System.out.println("Nodes of singly linked list:");
+		while(current != null) {
+			System.out.println(current.num+"");
+			current = current.next;
+		}
+		System.out.println();	
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Linked Problems");
+		
+		LinkedList list = new LinkedList();
+		
+		list.addNode(56);
+		list.addNode(30);
+		list.addNode(70);
+		
+		list.display();
 	}
 }
